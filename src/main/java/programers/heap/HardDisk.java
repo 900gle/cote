828c154b178f7c1680fb40ae08a7a04212cc752a
900gle.hupa <doo@iduhyeon-ui-MacBookPro-2.local>
2020-10-09 14:00:21 +0900

    hard disk

--- src/main/java/programers/heap/HardDisk.java
+++ src/main/java/programers/heap/HardDisk.java
@@ -1,12 +1,14 @@
 package programers.heap;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 
 public class HardDisk {
 
     public static void main(String[] args) {
 
-        int[][] jobs =  new int[][] {{0,3},{1,9}, {2,6}};
+        int[][] jobs = new int[][]{{0, 3}, {1, 9}, {2, 6}};
         System.out.println("ret :" + new HardDisk().solution(jobs));
     }
 
@@ -15,43 +17,52 @@ public class HardDisk {
         int sec;
         int work;
 
-        public Work (int sec, int work){
+        public Work(int sec, int work) {
             this.sec = sec;
             this.work = work;
         }
 
         @Override
-        public int compareTo(Work work) {
-            return this.work > work.work ? 1 : -1;
+        public int compareTo(Work o) {
+
+            if (this.work < o.work) return -1;
+            else if (this.work == o.work) {
+                if (this.sec < o.sec) return -1;
+                else return 1;
+            } else return 1;
+
         }
     }
 
     public int solution(int[][] jobs) {
 
+        int tempTime = 0;
+        int totalTime = 0;
         int answer = 0;
 
         PriorityQueue<Work> workPriorityQueue = new PriorityQueue<>();
-
-        for (int[] ints : jobs){
-
+        for (int[] ints : jobs) {
             workPriorityQueue.offer(new Work(ints[0], ints[1]));
         }
 
-        int total = 0;
-        int sum = 0;
-        while (!workPriorityQueue.isEmpty()){
-//            System.out.println(workPriorityQueue.peek().work);
-
-            total = (workPriorityQueue.peek().work) + total;
-
-            sum = total - workPriorityQueue.poll().sec;
-
-            System.out.println("total : "+total);
-
+        List<Work> list = new ArrayList<>();
+        while (!workPriorityQueue.isEmpty()) {
+            list.add(workPriorityQueue.poll());
         }
 
-        answer = sum / jobs.length;
+        while (list.size()>0) {
+            for (int i = 0; i < list.size(); i++) {
+                if (tempTime >= list.get(i).sec) {
+                    tempTime += list.get(i).work;
+                    totalTime += tempTime - list.get(i).sec;
+                    list.remove(i);
+                    break;
+                }
+                if (i == list.size()-1) totalTime++;
+            }
+        }
 
+        answer  = (int)totalTime/jobs.length;
         return answer;
     }
 }
